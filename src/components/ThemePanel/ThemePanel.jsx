fe3e601eb250705fb5c9a01ae5a74bed59f37ef4
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2025-04-03 22:00:26 +0300

    feat: подключен redux

--- src/components/ThemePanel/ThemePanel.jsx
+++ src/components/ThemePanel/ThemePanel.jsx
@@ -1,14 +1,19 @@
 import './ThemePanel.css'
+import { decrement, increment, incrementByAmount } from '../../store/counterSlice'
+import { useSelector, useDispatch } from 'react-redux'
 
 function ThemePanel() {
 
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
     const handleClick = (e) => console.log(e.target)
-
+    
     return (
         <nav className="theme-panel">
-            <button className="task-manager__button task-manager__button_theme_grey" onClick={handleClick}></button>
-            <button className="task-manager__button task-manager__button_theme_white"></button>
-            <button className="task-manager__button task-manager__button_theme_black"></button>
+            <button className="task-manager__button task-manager__button_theme_grey" onClick={() => dispatch(increment())}></button>
+            <button className="task-manager__button task-manager__button_theme_white"onClick={() => dispatch(decrement())}></button>
+            <button className="task-manager__button task-manager__button_theme_black"onClick={() => dispatch(incrementByAmount(40))}></button>
+            <p>counter: {count}</p>
         </nav>
     )
 }
